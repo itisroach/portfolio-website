c7e0e863a157cd0082530001eac04eb254b9f78d
unknown <rouchashoori@gmail.com>
2025-02-14 19:54:16 +0330

    fix background color

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@ AOS.init();
     <HeaderComp data-aos="fade-right" data-aos-duration="2000" />
     <Skills data-aos="fade-left" data-aos-duration="2000" />
   </section>
-  <div class="mt-72 flex justify-center items-center  relative">
+  <div class="lg:mt-72 mt-60 flex justify-center items-center  relative">
     <svg class="pointer" xmlns="http://www.w3.org/2000/svg" width="30" viewBox="0 0 320 512"><path fill="#ffffff" d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z"/></svg>
   </div>
   <Projects />
