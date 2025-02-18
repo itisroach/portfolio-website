94956d65aca805e0f0083e27638fe654708607c8
unknown <rouchashoori@gmail.com>
2025-02-18 22:06:33 +0330

    fix email address in footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
     <section class="flex flex-col justify-center items-center mt-10 text-justify w-80 gap-3 mx-auto">
         <h1 class="lg:text-3xl text-2xl lg:font-bold font-semibold text-white">Contact Me</h1>
         <p class="text-wrap lg:text-xl text-sm font-semibold text-center text-white">
-            Want to chat Or Share Your Thoughts About My Projects. Email me at <a class="hover:underline text-transparent relative bg-clip-text bg-gradient-to-r z-40 from-green-400 to-blue-400 font-bold" href="mailto:rouchashoori@gmail.com">rouchashoori@gmail.me</a> or message me on <a class="hover:underline text-transparent relative bg-clip-text bg-gradient-to-r z-40 from-green-400 to-blue-400 font-bold" href="https://linkedin.com/in/amirali-ashoori">Linkedin</a> Or <a class="hover:underline text-transparent relative bg-clip-text bg-gradient-to-r z-40 from-green-400 to-blue-400 font-bold" href="https://t.me/thisisroach">Telegram</a> and I'll respond as quick as possible.
+            Want to chat Or Share Your Thoughts About My Projects. Email me at <a class="hover:underline text-transparent relative bg-clip-text bg-gradient-to-r z-40 from-green-400 to-blue-400 font-bold" href="mailto:rouchashoori@gmail.com">rouchashoori@gmail.com</a> or message me on <a class="hover:underline text-transparent relative bg-clip-text bg-gradient-to-r z-40 from-green-400 to-blue-400 font-bold" href="https://linkedin.com/in/amirali-ashoori">Linkedin</a> Or <a class="hover:underline text-transparent relative bg-clip-text bg-gradient-to-r z-40 from-green-400 to-blue-400 font-bold" href="https://t.me/thisisroach">Telegram</a> and I'll respond as quick as possible.
         </p>
     </section>
 </template>
